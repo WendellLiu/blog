9b246a9e1fefc864a8d842da5b113fd2969687f8
Wendell Liu <cuk.bas@gmail.com>
2021-07-29 16:39:39 +0800

    feat: color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ const isProduction = process.env.NODE_ENV === 'production'
 const config = {
   variants: {},
   plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        main: {
+          DEFAULT: '#5DAC81',
+        },
+        cwhite: {
+          DEFAULT: '#FFFFFB',
+        },
+        second: {
+          DEFAULT: '#A0674B',
+        },
+        highlight: {
+          DEFAULT: '#D5E5F2',
+        },
+      },
+    },
+  },
 }
 
 if (isProduction) {
